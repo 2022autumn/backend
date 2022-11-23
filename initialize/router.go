4a97b31bebd8c3cf1cde765cc9e6df1f4aca2912
oouoouoo <89232603+oouoouoo@users.noreply.github.com>
2022-11-23 16:46:24 +0800

    update router

--- initialize/router.go
+++ initialize/router.go
@@ -27,8 +27,10 @@ func SetupRouter(r *gin.Engine) {
 	//用户模块
 	UserRouter := r.Group("api/v1")
 	{
-		UserRouter.POST("/register", v1.Register) //注册
-		UserRouter.POST("/login", v1.Login)       //登录
+		UserRouter.POST("/register", v1.Register)  //注册
+		UserRouter.POST("/login", v1.Login)        //登录
+		UserRouter.POST("/userinfo", v1.UserInfo)  //个人中心
+		UserRouter.POST("/usermod", v1.ModifyUser) //编辑个人信息
 	}
 	r.POST("/GetData", v1.GetData) //测试数据接收获取
 
