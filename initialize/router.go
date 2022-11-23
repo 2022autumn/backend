74983be5c32653b5e1e8f0fc4c0d87e4caf1f7b7
oouoouoo <89232603+oouoouoo@users.noreply.github.com>
2022-11-23 15:07:58 +0800

    change route

--- initialize/router.go
+++ initialize/router.go
@@ -1,6 +1,7 @@
 package initialize
 
 import (
+	v1 "IShare/api/v1"
 	"IShare/docs"
 	"IShare/middleware"
 	"net/http"
@@ -22,6 +23,14 @@ func SetupRouter(r *gin.Engine) {
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 
 	r.GET("/api/test", testGin)
+
+	//用户模块
+	UserRouter := r.Group("api/v1")
+	{
+		UserRouter.POST("/register", v1.Register) //注册
+		UserRouter.POST("/login", v1.Login)       //登录
+	}
+	r.POST("/GetData", v1.GetData) //测试数据接收获取
 
 	// baseGroup := r.Group("/api/v1")
 	// {
