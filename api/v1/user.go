c8d0c4db542def1daf7142772cbcaf2006809ea4
oouoouoo <89232603+oouoouoo@users.noreply.github.com>
2022-12-01 01:17:31 +0800

    add UploadHeadshot

--- api/v1/user.go
+++ api/v1/user.go
@@ -8,7 +8,9 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"golang.org/x/crypto/bcrypt"
+	"io"
 	"net/http"
+	"os"
 	"strconv"
 )
 
@@ -272,4 +274,85 @@ func ModifyPassword(c *gin.Context) {
 	})
 }
 
-//
+// UploadHeadshot 编辑用户信息
+// @Summary     ccf
+// @Description 上传用户头像
+// @Tags        用户
+// @Param       user_id query string true "user_id"
+// @Param       headshot  Request.FormFile file true "头像文件"
+// @Accept      json
+// @Produce     json
+// @Success     200 {string} json "{"status":200,"success":true,"msg":"修改成功","data":{object}}"
+// @Failure     200 {string} json "{"status":201,"success":false,"msg":"用户ID不存在"}"
+// @Failure     200 {string} json "{"status":202,"success":false,"msg":"头像文件上传失败"}"
+// @Failure     200 {string} json "{"status":203,"success":false,"msg":"文件保存失败"}"
+// @Failure     200 {string} json "{"status":204,"success":false,"msg":"保存文件路径到数据库中失败"}"
+// @Router      user/headshot [POST]
+func UploadHeadshot(c *gin.Context) {
+	userId := c.Query("user_id")
+	userID, _ := strconv.ParseUint(userId, 0, 64)
+	user, notFoundUserByID := service.QueryAUserByID(userID)
+	if notFoundUserByID {
+		c.JSON(http.StatusOK, gin.H{
+			"success": false,
+			"status":  201,
+			"msg":     "用户ID不存在",
+		})
+		return
+	}
+	//1、获取上传的文件
+	file, header, fileErr := c.Request.FormFile("headshot")
+	if fileErr != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"success": false,
+			"status":  202,
+			"msg":     "头像文件上传失败",
+		})
+		return
+	}
+	//2、将文件保存到本地
+	filePath := "./media/headshot/" + header.Filename
+	out, e := os.Create(filePath)
+	if e != nil {
+		fmt.Println(e)
+		_ = os.Mkdir(filePath, 777)
+		out, e = os.Create(filePath)
+		if e != nil {
+			fmt.Println(e)
+			c.JSON(http.StatusOK, gin.H{
+				"success": false,
+				"status":  203,
+				"msg":     "文件保存失败",
+			})
+		}
+		return
+	}
+	defer func(out *os.File) {
+		err := out.Close()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}(out)
+	_, err := io.Copy(out, file)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	//3、将文件对应路径更新到数据库中
+	user.HeadShot = filePath
+	err = global.DB.Save(user).Error
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"success": false,
+			"status":  204,
+			"msg":     "保存文件路径到数据库中失败",
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"success": true,
+		"msg":     "头像上传成功",
+		"status":  200,
+		"data":    user,
+	})
+}
