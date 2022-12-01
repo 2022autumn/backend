f6d4be7b1603777a62dab26cb32ed735957b3b1b
oouoouoo <89232603+oouoouoo@users.noreply.github.com>
2022-12-01 22:31:16 +0800

    update model.user

--- model/database/user.go
+++ model/database/user.go
@@ -11,10 +11,10 @@ type User struct {
 
 	UserType uint64 `gorm:"default:0" json:"user_type"` // 0: 普通用户，1: 认证机构用户,2 管理员
 
-	HeadShot    string `gorm:"size:64;" json:"head_shot"` //头像url
-	UserInfo    string `gorm:"size:64;" json:"user_info"` //个性签名
-	Name        string `gorm:"size:32;" json:"name"`      //真实姓名
-	Phone       string `gorm:"size:32;" json:"phone"`
+	HeadShot    string `gorm:"size:64;" json:"head_shot"`     //头像url
+	UserInfo    string `gorm:"size:64;" json:"user_info"`     //个性签名
+	Name        string `gorm:"size:32;" json:"name"`          //真实姓名
+	Phone       string `gorm:"size:32;" json:"phone"`         //电话号码
 	Email       string `gorm:"size:32;" json:"email"`         //邮箱
 	Fields      string `gorm:"size:256;" json:"fields"`       //研究领域
 	InterestTag string `gorm:"size:256;" json:"interest_tag"` //兴趣词
