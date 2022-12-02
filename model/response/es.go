ae06d03769d3a5ef0e8b3436bd76c480931bb1b7
horik <hr.mail.qaq@gmail.com>
2022-12-03 04:19:56 +0800

    fix(response):reset author relationship net struct

--- model/response/es.go
+++ model/response/es.go
@@ -33,17 +33,6 @@ type GetObjectA struct {
 }
 
 type AuthorRelationNet struct {
-	Vertex_set []struct {
-		id    string
-		label string
-	}
-	Edge_set []struct {
-		source string
-		target string
-		weight int
-		works  []struct {
-			id    string
-			title string
-		}
-	}
+	Vertex_set []map[string]interface{}
+	Edge_set   []map[string]interface{}
 }
