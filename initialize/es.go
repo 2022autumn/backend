985ce19c8d380d58ae72cc8e323fcc0ee15e5791
horik <hr.mail.qaq@gmail.com>
2022-12-04 12:01:52 +0800

    patch(init):refresh es.log

--- initialize/es.go
+++ initialize/es.go
@@ -14,11 +14,11 @@ import (
 
 func InitElasticSearch() {
 	host := global.VP.GetString("es.host")
-	// eslog, err := os.OpenFile(global.VP.GetString("es.logpath"),os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
-	// if err != nil {
-	// 	panic(fmt.Errorf("open es log file err: %v", err))
-	// 	return
-	// }
+	eslog, err := os.OpenFile(global.VP.GetString("es.logpath"), os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
+	if err != nil {
+		panic(fmt.Errorf("open es log file err: %v", err))
+		return
+	}
 	client, err := elastic.NewClient(
 		elastic.SetURL(host),
 		elastic.SetSniff(false),
@@ -27,7 +27,7 @@ func InitElasticSearch() {
 		//elastic.SetGzip(true),
 		elastic.SetErrorLog(log.New(os.Stderr, "ELASTIC ", log.LstdFlags)),
 		elastic.SetInfoLog(log.New(os.Stdout, "", log.LstdFlags)),
-		// elastic.SetTraceLog(log.New(eslog, "eslog", log.LstdFlags)),
+		elastic.SetTraceLog(log.New(eslog, "eslog", log.LstdFlags)),
 	)
 	if err != nil {
 		panic(fmt.Errorf("get esclient error: %s", err))
