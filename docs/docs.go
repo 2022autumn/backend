76ad09047d2d0c950000083d36f4ca380034bf97
oouoouoo <89232603+oouoouoo@users.noreply.github.com>
2022-12-09 12:54:11 +0800

    fix user(id-body+swag)

--- docs/docs.go
+++ docs/docs.go
@@ -202,7 +202,7 @@ const docTemplate = `{
         },
         "/login": {
             "post": {
-                "description": "登录",
+                "description": "登录\n填入用户名和密码",
                 "consumes": [
                     "application/json"
                 ],
@@ -226,19 +226,19 @@ const docTemplate = `{
                 ],
                 "responses": {
                     "200": {
-                        "description": "{\"status\":200,\"success\":true,\"msg\":\"login success\",\"token\": 666}",
+                        "description": "{\"status\":200,\"msg\":\"登录成功\",\"token\": token,\"ID\": user.UserID}",
                         "schema": {
                             "type": "string"
                         }
                     },
                     "400": {
-                        "description": "{\"status\":400,\"success\":false,\"msg\":\"username doesn't exist\"}",
+                        "description": "{\"status\":400,\"msg\":\"用户名不存在\"}",
                         "schema": {
                             "type": "string"
                         }
                     },
                     "401": {
-                        "description": "{\"status\":401,\"success\":false,\"msg\":\"password doesn't match\"}",
+                        "description": "{\"status\":401,\"msg\":\"密码错误\"}",
                         "schema": {
                             "type": "string"
                         }
@@ -248,7 +248,7 @@ const docTemplate = `{
         },
         "/register": {
             "post": {
-                "description": "填入用户名和密码注册",
+                "description": "注册\n填入用户名和密码注册",
                 "consumes": [
                     "application/json"
                 ],
@@ -272,13 +272,13 @@ const docTemplate = `{
                 ],
                 "responses": {
                     "200": {
-                        "description": "{\"status\":200,\"msg\":\"register success\"}",
+                        "description": "{\"status\":200,\"msg\":\"注册成功\"}",
                         "schema": {
                             "type": "string"
                         }
                     },
                     "400": {
-                        "description": "{\"status\":400,\"msg\":\"username exists\"}",
+                        "description": "{\"status\":400,\"msg\":\"用户名已存在\"}",
                         "schema": {
                             "type": "string"
                         }
@@ -485,6 +485,15 @@ const docTemplate = `{
                 "summary": "ccf",
                 "parameters": [
                     {
+                        "description": "data",
+                        "name": "data",
+                        "in": "body",
+                        "required": true,
+                        "schema": {
+                            "$ref": "#/definitions/response.AvatarQ"
+                        }
+                    },
+                    {
                         "type": "file",
                         "description": "新头像",
                         "name": "Headshot",
@@ -550,13 +559,13 @@ const docTemplate = `{
                 ],
                 "responses": {
                     "200": {
-                        "description": "{\"status\":200,\"msg\":\"get info of user\",\"data\":{object}}",
+                        "description": "{\"status\":200,\"msg\":\"获取用户信息成功\",\"data\":{object}}",
                         "schema": {
                             "type": "string"
                         }
                     },
                     "400": {
-                        "description": "{\"status\":400,\"msg\":\"userID not exist\"}",
+                        "description": "{\"status\":400,\"msg\":\"用户ID不存在\"}",
                         "schema": {
                             "type": "string"
                         }
@@ -579,13 +588,6 @@ const docTemplate = `{
                 "summary": "ccf",
                 "parameters": [
                     {
-                        "type": "string",
-                        "description": "user_id",
-                        "name": "user_id",
-                        "in": "query",
-                        "required": true
-                    },
-                    {
                         "description": "data",
                         "name": "data",
                         "in": "body",
@@ -597,7 +599,7 @@ const docTemplate = `{
                 ],
                 "responses": {
                     "200": {
-                        "description": "{\"status\":200,\"msg\":\"修改成功\",\"data\":{object}}",
+                        "description": "{\"status\":200,\"msg\":\"修改个人信息成功\",\"data\":{object}}",
                         "schema": {
                             "type": "string"
                         }
@@ -632,30 +634,18 @@ const docTemplate = `{
                 "summary": "ccf",
                 "parameters": [
                     {
-                        "type": "string",
-                        "description": "user_id",
-                        "name": "user_id",
-                        "in": "query",
-                        "required": true
-                    },
-                    {
-                        "type": "string",
-                        "description": "旧密码",
-                        "name": "Password_Old",
-                        "in": "formData",
-                        "required": true
-                    },
-                    {
-                        "type": "string",
-                        "description": "新密码",
-                        "name": "Password_New",
-                        "in": "formData",
-                        "required": true
+                        "description": "data",
+                        "name": "data",
+                        "in": "body",
+                        "required": true,
+                        "schema": {
+                            "$ref": "#/definitions/response.PwdModifyQ"
+                        }
                     }
                 ],
                 "responses": {
                     "200": {
-                        "description": "{\"status\":200,\"msg\":\"修改成功\",\"data\":{object}}",
+                        "description": "{\"status\":200,\"msg\":\"修改密码成功\",\"data\":{object}}",
                         "schema": {
                             "type": "string"
                         }
@@ -731,6 +721,17 @@ const docTemplate = `{
                         "type": "object",
                         "additionalProperties": true
                     }
+                }
+            }
+        },
+        "response.AvatarQ": {
+            "type": "object",
+            "required": [
+                "user_id"
+            ],
+            "properties": {
+                "user_id": {
+                    "type": "string"
                 }
             }
         },
@@ -829,6 +830,9 @@ const docTemplate = `{
         },
         "response.ModifyQ": {
             "type": "object",
+            "required": [
+                "user_id"
+            ],
             "properties": {
                 "email": {
                     "description": "邮箱",
@@ -850,8 +854,32 @@ const docTemplate = `{
                     "description": "电话号码",
                     "type": "string"
                 },
+                "user_id": {
+                    "type": "string"
+                },
                 "user_info": {
                     "description": "个性签名",
+                    "type": "string"
+                }
+            }
+        },
+        "response.PwdModifyQ": {
+            "type": "object",
+            "required": [
+                "password_new",
+                "password_old",
+                "user_id"
+            ],
+            "properties": {
+                "password_new": {
+                    "type": "string",
+                    "minLength": 6
+                },
+                "password_old": {
+                    "type": "string",
+                    "minLength": 6
+                },
+                "user_id": {
                     "type": "string"
                 }
             }
